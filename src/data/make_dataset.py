a5eb78c6f10998dc548617ad4d465ecfc0ac37ed
Psalishol Samuel <psalishol80@gmail.com>
2021-12-06 15:34:52 -0800

    Nothing was done here

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -5,6 +5,7 @@ from pathlib import Path
 from dotenv import find_dotenv, load_dotenv
 
 
+
 @click.command()
 @click.argument('input_filepath', type=click.Path(exists=True))
 @click.argument('output_filepath', type=click.Path())
@@ -14,7 +15,6 @@ def main(input_filepath, output_filepath):
     """
     logger = logging.getLogger(__name__)
     logger.info('making final data set from raw data')
-
 
 if __name__ == '__main__':
     log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
