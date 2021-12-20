c73e9008777f30a173f45fab0e1e7199d43cc81b
Psalishol Samuel <psalishol80@gmail.com>
2021-12-20 08:13:42 -0800

    Added script for preprocessing

--- src/data/preprocessing.py
+++ src/data/preprocessing.py
@@ -1,40 +1,63 @@
-from pyspark.sql import SparkSession
-from pyspark.sql.types import *
+import pandas as pd
+import numpy as np
+from partition import *
 
+def clean_data(data):
+    """Removing some irrelevant columns in the dataframe"""
 
-# Making a sparksession
-SPARK_SESSION = SparkSession \
-    .builder \
-    .appName("Preprocessing with Spark") \
-    .getOrCreate()
+    # Defining some columns to remove
+    cols = ["vin", 'description', "exterior_color", "wheel_system", "vehicle_damage_category", "trimId",
+            "theft_title", "sp_id", "main_picture_url", "longitude", "listing_id", "listing_color", "latitude",
+                "interior_color", "cabin", "major_options", "back_legroom", "bed", "bed_height", "bed_length", "is_certified",
+                    "is_cpo", "is_oemcpo", "salvage", "wheelbase", "width","combine_fuel_economy","daysonmarket","dealer_zip","engine_cylinders",
+                    "franchise_dealer","front_legroom","fuel_tank_volume","height","length","franchise_make","savings_amount","transmission_display","trim_name"
+                      ]   
+    # Dropping the columns
+    data = data.drop(columns=cols)
+    # data = data.drop(cols, axis=1)
+    
+    # listed date to pandas datetime
+    data["listed_date"] = pd.to_datetime(data["listed_date"])
+    
+    # For transforming power and torque
+    def torque(feature):
+        if feature == None:
+            pass
+        else:
+            int(feature.split("@")[0].strip().split(" ")[0])
+    def transmission(feature):
+        if feature == None:
+            pass
+        else:
+            feature.replace("A","Automatic").replace("M","Manual")
+    def max_seating(feature):
+        if feature == None:
+            pass
+        else:
+            int(feature.strip().split(" ")[0])
+            
+    # Transforming
+    data["transmission"] = data["transmission"].apply(transmission)
+    data["power"] = data["power"].apply(torque)
+    data["torque"] = data["torque"].apply(torque)
+    data["maximum_seating"] = data["maximum_seating"].apply(max_seating)
+    data["Listing_year"] = data["listed_date"].apply(lambda inf: inf.year)
+    data["engine_displacement"] = data["engine_displacement"].apply(lambda inf: inf/1000)
     
     
-
-# Defining a schema for the variables
-SCHEMA = StructType([
-    StructField("id", IntegerType(), nullable=False),
-    StructField("region", StringType(), nullable=False),
-    StructField("price", IntegerType(), nullable=False),
-    StructField("manufacturer", StringType(), nullable=False),
-    StructField("model", StringType(), nullable=False),
-    StructField("condition", StringType(), nullable=False),
-    StructField("cylinders", StringType(), nullable=False),
-    StructField("fuel", StringType(), nullable=False),
-    StructField("odometer", IntegerType(), nullable=False),
-    StructField("transmission", StringType(), nullable=False),
-    StructField("drive", StringType(), nullable=False),
-    StructField("size", StringType(), nullable=False),
-    StructField("type", StringType(), nullable=False),
-    StructField("state", StringType(), nullable=False),
-    StructField("lat", IntegerType(), nullable=False),
-    StructField("long", IntegerType(), nullable=False),
-
-])
-
-
-
+    # Renaming some columns
+    data = data.rename(columns={"wheel_system_display":"Drivetrain",
+                                "transmission":"Transmission",
+                                "body_type":"body_style",
+                                "city_fuel_economy":"city_MPG",
+                                "engine_displacement":"engine_size",
+                                "highway_fuel_economy":"highway_MPG"})
+    
+    return data
+    
+    
 if __name__ == '__main__':
-    # Reading the data
-    df = SPARK_SESSION.read.csv(
-        r"..\data\external\vehicles.csv", header=True, schema=SCHEMA)
-    print(df)
+    DATAPATH = r"file path here"
+    data = pd.read_csv(DATAPATH)
+    # Cleaning the data
+    cleanData = clean_data(data)
